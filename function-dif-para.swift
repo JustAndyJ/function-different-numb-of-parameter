3d970a4615f54aa3145df66d6be6f112aa946bf9
Andy Bryan Julca Tello <julcac137@gmail.com>
2023-12-23 17:04:29 -0500

    first commit

---
+++ function-dif-para.swift
@@ -0,0 +1,16 @@
+// function with two parameters
+func display(number1: Int, number2: Int) {
+   print("1st Integer: \(number1) and 2nd Integer: \(number2)")
+}
+
+// function with a single parameter
+func display(number: Int) {
+   print("Integer number: \(number)")
+}
+
+
+// function call with single parameter
+display(number: 5)
+
+// function call with two parameters
+display(number1: 6, number2: 8)
